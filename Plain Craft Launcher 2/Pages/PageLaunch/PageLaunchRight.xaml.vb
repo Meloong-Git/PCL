092b2a2edd34402ee6e6ee00de8791ca22f2c048
Zhengyuan Huang <neclyon@qq.com>
2026-08-25 02:04:56 +0800

    fix: 对不起啦，忘记改ID会影响仓库地址了

--- Plain Craft Launcher 2/Pages/PageLaunch/PageLaunchRight.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLaunch/PageLaunchRight.xaml.vb
@@ -89,7 +89,7 @@ Public Class PageLaunchRight
                         Url = "https://files.mcimirror.top/PCL"
                     Case 11
                         Logger.Info("主页预设：杂志主页")
-                        Url = "https://gh-proxy.com/https://github.com/CreeperIsASpy/Magazine-Homepage-PCL/raw/main/output/Custom.xaml"
+                        Url = "https://gh-proxy.com/https://github.com/Neclyon/Magazine-Homepage-PCL/raw/main/output/Custom.xaml"
                     Case 12
                         Logger.Info("主页预设：PCL GitHub 仪表盘")
                         Url = "https://ddf.pcl-community.top/Custom.xaml"
